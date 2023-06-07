c7954bbd4027377ab65c3b059a07a29eb2e2b9f3
Daniel Ozoani <57331891+OzunaModelo123@users.noreply.github.com>
2023-06-07 13:09:38 -0400

    added an array tester to check the average array

--- ArrayTools.kt
+++ ArrayTools.kt
@@ -1,4 +1,6 @@
-
+fun main() {
+    averageArrayTester()
+}
 fun arrayAverage(inputArray: Array<Int>): Double {
     if (inputArray.isEmpty()) { // Avoid divide by 0 in return
         return 0.0
@@ -10,3 +12,10 @@ fun arrayAverage(inputArray: Array<Int>): Double {
     return sum / inputArray.size
 }
 
+fun averageArrayTester() {
+    println(arrayAverage(arrayOf(1, 2, 3))) // Except: 2
+    println(arrayAverage(arrayOf(-1, 2, -3))) // Expect: -2/3
+    println(arrayAverage(arrayOf(-1909))) // Expect: -1909
+    println(arrayAverage(arrayOf(-0))) // Expect: 0
+    println(arrayAverage(arrayOf()))
+}
